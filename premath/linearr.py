b21ada0c306fd5b3eabd065fa1af5f38b1e93fce
Iñaki Salcedo <67810965+inakisalcedo@users.noreply.github.com>
2024-05-16 17:37:26 -0400

    Update linearr.py

--- premath/linearr.py
+++ premath/linearr.py
@@ -3,9 +3,9 @@ from sklearn.linear_model import LinearRegression
 from scipy.stats import pearsonr
 
 class LinearR:
-    ''' Linear Regression Class '''
+    # Linear Regression Class
     def __init__(self, x_data, y_data):
-        ''' Initialization '''
+        # Initialization
         x = np.array(x_data).reshape(-1, 1)
         y = np.array(y_data)
         modelo = LinearRegression()
@@ -14,27 +14,27 @@ class LinearR:
         self.mx = float(modelo.coef_)
         self.bx = modelo.intercept_
     def predicty(self, x_pred):
-        ''' Prediction of 'y' based on 'x' '''
+        # Prediction of 'y' based on 'x'
         predicty = self.mx * x_pred + self.bx
         return predicty
     def predictx(self, y_pred):
-        ''' Prediction of 'x' based on 'y' '''
+        # Prediction of 'x' based on 'y'
         predictx = (y_pred - self.bx) / self.mx
         return predictx
     def r(self):
-        ''' Pearson correletion coeficient '''
+        # Pearson correletion coeficient
         return self.corr
     def r2(self):
-        ''' Determination coeficient '''
+        # Determination coeficient
         return self.corr ** 2
     def m(self):
-        ''' Slope '''
+        # Slope
         return self.mx
     def b(self):
-        ''' Intercept '''
+        # Intercept
         return self.bx
     def eq(self):
-        ''' Linear ecuation in the form 'y = mx + b' '''
+        # Linear ecuation in the form 'y = mx + b'
         if self.bx > 0:
             s = '+'
         elif self.bx < 0:
